3cfe813ee966c231e0b1e6297956c084717fde98
um-joon-sik <39338850+kimduuukbae@users.noreply.github.com>
2025-05-11 04:05:48 +0900

    initial

---
+++ DynamicSky/Source/DynamicSky/DynamicSky.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DynamicSky.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DynamicSky, "DynamicSky" );
---
+++ DynamicSky/Source/DynamicSky/DynamicSkyGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DynamicSkyGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DYNAMICSKY_API ADynamicSkyGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
